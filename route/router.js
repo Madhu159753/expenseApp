9e5fb0a812989357a21736c824126635db1bd5ce
Madhu159753 <madhu01111998@gmail.com>
2023-01-17 00:42:28 +0530

    login page

--- route/router.js
+++ route/router.js
@@ -4,21 +4,29 @@ const router=express.Router();
 const rootDir=require('../util/path');
 const logindata=require('../model/logindata');
 router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'view','login.html'));
+    res.sendFile(path.join(rootDir,'view','signin.html'));
 });
-router.post('/user-login',async(req,res,next)=>{
+router.post('/user-signin',async(req,res,next)=>{
      try{
      const name=req.body.name;
      const email=req.body.email;
      const password=req.body.password;
-     const data=await logindata.create({name:name,email:email,password:password});
-     res.status(201).json({Details:data});
+     if(name=='undefined'|| name===0 
+        || password=='null'|| password===0
+        || email=='null' || email===0){
+            return res.status(400).json({err:'bad parameer, something is missing'})
+        }
+     await logindata.create({name:name,email:email,password:password});
+     res.status(201).json({Details:'successfuly creaed new user'});
      }
      catch(err){
         console.log(err);
-        res.send(500).json({
+        res.sendStatus(500).json({
             error:err
         })
      }
 });
+router.get('/user-login',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'view','logIn.html'));
+})
 module.exports=router;
