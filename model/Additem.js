e2563942f5d56d090f6e4082d8bda1c429b89b1b
Madhu159753 <madhu01111998@gmail.com>
2023-01-18 02:07:57 +0530

    add delete expense

---
+++ model/Additem.js
@@ -0,0 +1,23 @@
+const Sequelize=require('sequelize');
+const sequelize=require('../util/database');
+const Additem=sequelize.define('additem',{
+    id:{
+        type:Sequelize.INTEGER,
+        autoIncrement:true,
+        unique:true,
+        primaryKey:true,
+        allowNull:false
+    },
+    description:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    choose:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    amount:{
+        type:Sequelize.INTEGER,
+        allowNull:false
+    },
+});
