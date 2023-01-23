6754dc23c83cfe9ac959fd41bcdcae2a7eeaf400
Madhu159753 <madhu01111998@gmail.com>
2023-01-23 20:21:39 +0530

    optimization

--- controller/premiumFeaure.js
+++ controller/premiumFeaure.js
@@ -1,31 +1,27 @@
 //const express=require('express');
 const logindata=require('../model/logindata');
 const Additem=require('../model/Additem');
-//const sequelize=require('../util/database');
+const sequelize=require('../util/database');
 
 exports.getUserLeaderBoard=async (req,res)=>{
     try{
-       const users=await logindata.findAll()
-       const expenses=await Additem.findAll();
-       const userAggregatedExpenses={}
-       expenses.forEach((expense )=> {
-        if(userAggregatedExpenses[expense.loginId]){
-         userAggregatedExpenses[expense.loginId]=userAggregatedExpenses[expense.loginId]+expense.amount
-        }else
-        {
-            userAggregatedExpenses[expense.loginId]=expense.amount
-        }
-       })
-     
-      userLeaderBoardDetails=[];
-      users.forEach((user)=>{
-        userLeaderBoardDetails.push({name:user.name, total_cost:userAggregatedExpenses[user.id]||0 })
-      })
-     userLeaderBoardDetails.sort((a,b)=> b.total_cost-a.total_cost )
-      res.status(200).json(userLeaderBoardDetails)
+       const users=await logindata.findAll({
+         attributes:['id','name',[sequelize.fn('sum',sequelize.col('additems.amount')),'total_cost']],
+         include:[
+            {
+            model:Additem,
+            attributes:[]
+         }
+        ],
+        group:['login.Id'],
+        order:[['total_cost','DESC']]
+       });
+
+       
+      res.status(200).json(users)
      }
     catch(err){
-        //console.log(err);
+       console.log(err);
         res.status(500).json(err)
     }
 }
