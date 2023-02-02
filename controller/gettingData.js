37bf69816b1e4ef96a59130a906a2144a4a60c61
Madhu159753 <madhu01111998@gmail.com>
2023-02-02 19:37:49 +0530

    adding paginaion

--- controller/gettingData.js
+++ controller/gettingData.js
@@ -1,20 +1,27 @@
-const logindata=require('../model/logindata');
 const Additem=require('../model/Additem');
-const jwt=require('jsonwebtoken');
+const ITEMS_PER_PAGE=2;
+exports.gettingDataFromExpense=(req,res,next)=>{
+    console.log('getting expenses')
+    let page=+req.query.page||1;
+     let totalItems;
+     Additem.count().then((total)=>{
+        totalItems=total;
+        return Additem.findAll({offset:(page-1)*ITEMS_PER_PAGE,limit:ITEMS_PER_PAGE})
+     })
+      .then((products)=>{
+        res.json({
+            products:products,
+            currentPage:page,
+            hasNextPage:ITEMS_PER_PAGE*page<totalItems,
+            nextPage:page+1,
+            hasPreviousPage:page>1,
+            previousPage:page-1,
+            lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
 
-exports.gettingDataFromExpense=async(req,res,next)=>{
-    try{
-    const user=await Additem.findAll({where:{loginId:req.user.id}});
-     return  res.status(201).json({allData:user});
-    }
-    catch(err)
-    {
-        console.log(err);
-        res.status(500).json({
-            Error:err
         })
+      })
+
     }
-}
 exports.deletedata=async(req,res)=>{
     try{
      if(req.params.id=='undefined'){
@@ -33,3 +40,16 @@ exports.deletedata=async(req,res)=>{
      })
     }
  }
+//  exports.gettingDataOfSingleUser=async(req,res,next)=>{
+//     try{
+//     const user=await Additem.findAll({where:{loginId:req.user.id}});
+//      return  res.status(201).json({allData:user});
+//     }
+//     catch(err)
+//     {
+//         //console.log(err);
+//         res.status(500).json({
+//             Error:err
+//         })
+//     }
+// }
